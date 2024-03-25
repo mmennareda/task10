8a43dd9f0a8c8d7aa9e2e78417ea416ee1a58592
mmennareda <163691031+mmennareda@users.noreply.github.com>
2024-03-25 10:31:59 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var counter=0;
+function increment() {
+    counter++;
+    document.getElementById("number").innerHTML= counter;
+}
+
+function decrement() {
+    counter--;
+    document.getElementById("number").innerHTML= counter;
+    
+}
